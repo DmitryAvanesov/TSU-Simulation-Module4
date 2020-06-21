cd9900e1611136c1fcfa4ea5b2e26a2dc4d668aa
Dima <dima.avanesov@yandex.ru>
2020-06-21 21:54:58 +0700

    Exponential distribution for staff time

--- src/app/agent-based/agent-based.component.ts
+++ src/app/agent-based/agent-based.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+interface IContext {
+  staffIntensity: number
+}
+
 interface IAgentCustomer {
   requestSize: number,
   isScandalous: boolean
@@ -9,7 +13,7 @@ interface IAgentEmployee {
   handleSpeed: number,
   customer: IAgentCustomer,
   timePassed: number,
-  getTime(): number
+  getTime(context: IContext): number
 }
 
 @Component({
@@ -23,25 +27,27 @@ export class AgentBasedComponent implements OnInit {
   staff: Array<IAgentEmployee>;
   queue: Array<IAgentCustomer>;
   queueIntensity: number;
+  staffIntensity: number;
 
   ngOnInit(): void {
-    this.numberOfStaff = 18;
+    this.numberOfStaff = 20;
     this.staff = new Array(this.numberOfStaff);
     this.queue = new Array();
-    this.queueIntensity = 0.001;
+    this.queueIntensity = 0.00075;
+    this.staffIntensity = 0.00005;
 
     for (const [index] of this.staff.entries()) {
       this.staff[index] = {
         handleSpeed: Math.random() / 2 + 0.5,
         customer: undefined,
         timePassed: 0,
-        getTime: function () {
+        getTime: function (context) {
           if (this.customer) {
             if (this.customer.isScandalous) {
-              return (this.handleSpeed + this.customer.requestSize) * 50000 - this.timePassed;
+              return -Math.log(Math.random()) / context.staffIntensity * (this.handleSpeed + this.customer.requestSize) * 5 - this.timePassed;
             }
 
-            return (this.handleSpeed + this.customer.requestSize) * 10000 - this.timePassed;
+            return -Math.log(Math.random()) / context.staffIntensity * (this.handleSpeed + this.customer.requestSize) - this.timePassed;
           }
 
           return 0;
@@ -72,7 +78,7 @@ export class AgentBasedComponent implements OnInit {
     let nextAgent = 0;
 
     for (const [index, value] of this.staff.entries()) {
-      const curTime = value.getTime();
+      const curTime = value.getTime(this);
 
       if (curTime < minTime) {
         minTime = curTime;
