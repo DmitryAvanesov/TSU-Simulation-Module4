2524f2bb605bf14dbd4ba3008d2ead7044550473
Dima <dima.avanesov@yandex.ru>
2020-06-21 19:19:28 +0700

    Queue-staff interaction works!

--- src/app/agent-based/agent-based.component.ts
+++ src/app/agent-based/agent-based.component.ts
@@ -37,7 +37,7 @@ export class AgentBasedComponent implements OnInit {
         timePassed: 0,
         getTime: function () {
           if (this.customer) {
-            return this.handleSpeed + this.customer.requestSize - this.timePassed;
+            return (this.handleSpeed + this.customer.requestSize) * 10000 - this.timePassed;
           }
 
           return 0;
@@ -57,14 +57,13 @@ export class AgentBasedComponent implements OnInit {
           isScandalous: Math.random() < 0.1
         });
 
-        console.log('queue', this.queue.length)
         this.fillQueue();
       },
       -Math.log(Math.random()) / this.queueIntensity
     );
   }
 
-  getNextEvent() {
+  getNextEvent(curAgent: number = undefined) {
     let minTime = 10e10;
     let nextAgent = 0;
 
@@ -77,10 +76,10 @@ export class AgentBasedComponent implements OnInit {
       }
     }
 
-    console.log('time', minTime)
-
-    for (const [index] of this.staff.entries()) {
-      this.staff[index].timePassed += minTime;
+    for (const [index, value] of this.staff.entries()) {
+      if (value.customer) {
+        this.staff[index].timePassed += minTime;
+      }
     }
 
     setTimeout(
@@ -89,12 +88,12 @@ export class AgentBasedComponent implements OnInit {
           this.staff[nextAgent].timePassed = 0;
           this.staff[nextAgent].customer = this.queue[0];
           this.queue.shift();
-
-          console.log(nextAgent, 'agent')
-          console.log('queue', this.queue.length)
+        }
+        else {
+          this.staff[nextAgent].customer = undefined;
         }
 
-        this.getNextEvent();
+        this.getNextEvent(nextAgent);
       },
       minTime
     );
